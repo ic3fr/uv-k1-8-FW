334308c80edc6db41ff5f0f5f76724fdb392253a
ic3rick4g <150446918+ic3fr@users.noreply.github.com>
2025-12-10 00:10:35 +0100

    Update GameMorse.h

--- App/app/GameMorse.h
+++ App/app/GameMorse.h
@@ -2,3 +2,30 @@
  *   game to learn morse on uvK1
  *
 */
+include "keyboard_state.h"
+
+#include "../bitmaps.h"
+#include "../board.h"
+#include "py32f0xx.h"
+#include "../driver/bk4819-regs.h"
+#include "../driver/bk4819.h"
+#include "../driver/gpio.h"
+#include "../driver/keyboard.h"
+#include "../driver/st7565.h"
+#include "../driver/system.h"
+#include "../driver/systick.h"
+#include "../external/printf/printf.h"
+#include "../font.h"
+#include "../helper/battery.h"
+#include "../misc.h"
+#include "../radio.h"
+#include "../settings.h"
+#include "../ui/helper.h"
+#include "../audio.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
